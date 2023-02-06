75c0571ddc63f770846e28db77d3857ed4574457
Darshan-webapp <darshanpurohit1803@gmail.com>
2023-02-06 19:58:01 +0530

    added navbar

--- door.js
+++ door.js
@@ -1,12 +1,25 @@
   const doorImage = document.getElementById("image1");
   const openButton = document.querySelector(".btn1");
   const closeButton = document.querySelector(".btn2");
+  const statusDisplay = document.querySelector(".status");
+
+  let doorStatus = "closed";
 
   openButton.addEventListener("click", function() {
     doorImage.src = "images/Image2.png";
+    doorStatus = "Open";
+    statusDisplay.innerHTML = "Door is Open";
+    statusDisplay.style.color = "green";
+    
   });
 
   closeButton.addEventListener("click", function() {
     doorImage.src = "images/Image1.png";
-  });
+    doorStatus = "Closed";
+    statusDisplay.innerHTML = "Door is Closed";
+    statusDisplay.style.color = "red";
+    });
+
+  
+  
 
